55016bd5184e45bf5343cd172c41c7d266af2303
John Kamenik <jkamenik@gmail.com>
2012-02-12 22:21:52 -0500

    Finally got the tree working

--- app/view/layout/Nav.js
+++ app/view/layout/Nav.js
@@ -5,19 +5,21 @@ Ext.define('WebUI.view.layout.Nav',{
   split: true,
   rootVisible: false,
   
-  fields: ['text','link','panel'],
-  
-  root: {
-    expanded: true, 
-    text:"",
-    user:"",
-    status:"",
-    children: [
-      { text:'Google', link: 'http://google.com', leaf: true},
-      { text: 'Interfaces', expanded: true, children: [
-        {text: 'Ethernet', panel: 'interface-ethernet', leaf: true},
-        {text: 'IP', panel: 'interface-ip', leaf: true}
-      ]}
-    ]
-  }
+  // this is a mojor bug in ExtJS that cannot be fixed.  Instead of being able to load a store from a file you must
+  // actually instantiate a full store before the tree panel is instantiated.
+  store: Ext.create('Ext.data.TreeStore', {
+    fields: ['text','link','panel'],
+
+    root: {
+      expanded: true, 
+      text:"root",
+      children: [
+        { text:'Google', link: 'http://google.com', leaf: true},
+        { text: 'Interfaces', expanded: true, children: [
+          {text: 'Ethernet', panel: 'interface-ethernet', leaf: true},
+          {text: 'IP', panel: 'interface-ip', leaf: true}
+        ]}
+      ]
+    }
+  })
 });
