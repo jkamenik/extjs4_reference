8f90de29f2ce2e4bd44d9e9d14dd68356baf5483
John Kamenik <jkamenik@gmail.com>
2012-03-20 07:54:10 -0400

    Switching to an internal store which should allow for associations

--- app/store/Ethernets.js
+++ app/store/Ethernets.js
@@ -2,10 +2,10 @@ Ext.define('WebUI.store.Ethernets', {
   extend:  'Ext.data.Store',
   model:   'WebUI.model.Ethernet',
 
-  data: [
-      { name: 'Ethernet 1' },
-      { name: 'Ethernet 2' },
-      { name: 'Ethernet 3' },
-      { name: 'Ethernet 4' }
-  ]
+  proxy: {
+    type: 'localstorage',
+    id  : 'ethernets'
+  },
+  autoLoad: true,
+  autoSync: true
 });
--- app/store/Ips.js
+++ app/store/Ips.js
@@ -2,10 +2,10 @@ Ext.define('WebUI.store.Ips', {
   extend:  'Ext.data.Store',
   model:   'WebUI.model.Ip',
 
-  data: [
-      { ip: '1.1.1.1', mask: '255.255.255.0' },
-      { ip: '2.2.2.2', mask: '255.255.255.0' },
-      { ip: '3.3.3.3', mask: '255.255.255.0' },
-      { ip: '4.4.4.4', mask: '255.255.255.0' }
-  ]
+  proxy: {
+    type: 'localstorage',
+    id  : 'ips'
+  },
+  autoLoad: true,
+  autoSync: true
 });
